1a31db2e576e2616cc62e443f62beb1a7e2297ae
Super Sonic <stevenscotttucker@gmail.com>
2019-04-07 20:38:56 -0400

    edits

--- public/js/index.js
+++ public/js/index.js
@@ -196,7 +196,7 @@ function getCompanyLookupName() {
   if (!place.geometry) {
     // User entered the name of a Place that was not suggested and
     // pressed the Enter key, or the Place Details request failed.
-    window.alert("No details available for input: '" + place.name + "'");
+    window.alert("That company is not in our database please try another company: '" + place.name + "'");
     return;
   }
 
